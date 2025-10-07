7fc2dc207ae10ee9f43a626eff5e9d2c5a63f966
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-10-07 11:37:36 +0200

    Created data-cleaning function.

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ export async function fetchWeather(location) {
         console.error('API error:', error); 
     }
 }
+
+export function processWeatherData(data) {
+    const current = data.currentConditions; 
+    return {
+        location: data.resolvedAddress, 
+        temperature: current.temp, 
+        conditions: current.conditions,  
+        humidity: current.humidity, 
+        windspeed: current.windspeed
+    }; 
+}
